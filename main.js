175b5c089b57f2640264c436023628e20925e24d
Diana Laverde <juanangel110923@gmail.com>
2021-07-06 18:02:07 -0500

    finalize section footer

---
+++ main.js
@@ -0,0 +1,9 @@
+var github = document.getElementsByClassName("github");
+github[0].addEventListener("click", () => {
+  window.open("https://github.com/DianaLaverde", "_blank");
+});
+
+var linkedin = document.getElementsByClassName("linkedin");
+linkedin[0].addEventListener("click", () => {
+  window.open("https://github.com/DianaLaverde", "_blank");
+});
